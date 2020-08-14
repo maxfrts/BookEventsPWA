9cf692900abb9505818f20ca4ec6849599eab44b
Maxwell Freitas <maxfrts@gmail.com>
2020-08-13 22:03:09 -0300

    initial commit

---
+++ Models/EventModel.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace bookEventsPWA.Models
+{
+    public class EventModel
+    {
+        public EventTypeModel EventType { get; set; }
+
+        public DateTime EventDate {get; set;}
+
+        public string Description {get;set;}
+
+        public string Summary {get; set;}
+
+        public int TicketsForSale {get; set;}
+
+        public string Location {get;set;}
+
+        public double TicketPrice {get; set;}
+    }
+}
---
+++ Models/EventTypeModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace bookEventsPWA.Models
+{
+    public class EventTypeModel
+    {
+        public int EventTypeId { get; set; }
+
+        public string EventTypeDescription  { get; set; }
+    }
+}
